6d14ca8d28847cee56e819a807e20a10f7969a63
httpjamesm <github@httpjames.space>
2021-06-10 19:45:59 -0400

    tostring encoded data before decoding

--- src/pages/Chat.js
+++ src/pages/Chat.js
@@ -56,7 +56,7 @@ const b64toBlob = (b64Data, contentType='', sliceSize=512) => {
 };
 
 function decodeB64Blob(encodedBlobData) {
-    var array = encodedBlobData.split(",");
+    var array = encodedBlobData.toString().split(",");
     array[1] = atob(array[1]);
     return array.join(',')
 }
