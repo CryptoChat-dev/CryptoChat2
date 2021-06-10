1b8b64176725fcff294307da93a75092964cf3c5
httpjamesm <github@httpjames.space>
2021-06-10 19:46:51 -0400

    btoa first

--- src/pages/Chat.js
+++ src/pages/Chat.js
@@ -57,7 +57,7 @@ const b64toBlob = (b64Data, contentType='', sliceSize=512) => {
 
 function decodeB64Blob(encodedBlobData) {
     var array = encodedBlobData.toString().split(",");
-    array[1] = atob(array[1]);
+    array[1] = atob(btoa(array[1]));
     return array.join(',')
 }
 
